caaf01209c1cfeeeeab6596b246e7231461a596e
BradleyLangguth <35529740+BradleyLangguth@users.noreply.github.com>
2018-08-11 22:30:25 +0200

    Update 3.2
    Toevoeging van de FSM

---
+++ Assets/Code/Script_FSM.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_FSM : MonoBehaviour 
+{
+
+    private float startTime;
+
+    public string state = "NEUTRAL";
+    
+    
+    
+    
+    void Start () 
+    {
+        startTime = Time.timeSinceLevelLoad;
+	}
+    
+	void Update () 
+    {
+		switch (state)  
+        {
+            case "NEUTRAL":
+            
+            
+                if (Time.timeSinceLevelLoad > 10)
+                {
+                    state = "ATTACK";
+                }
+        
+                break;
+        
+        
+            case "ATTACK":
+            
+                Debug.Log("ATTACK");
+        
+                break;
+        }
+	}
+}
